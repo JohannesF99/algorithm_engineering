35dab0049a01f08b47c1e067a99ef161e7770c1b
Johannes Franke <jofra@hotmail.de>
2023-11-20 23:56:53 +0100

    Fizzbuzz library

---
+++ answers/05_CMake_and_Optimization_Process/02_fizzbuzz/catch_main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by johannes on 20.11.23.
+//
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_FAST_COMPILE
+
+#include "catch/include/catch.hpp"
---
+++ answers/05_CMake_and_Optimization_Process/02_fizzbuzz/fizzbuzz/include/fizzbuzz.h
@@ -0,0 +1,12 @@
+//
+// Created by johannes on 20.11.23.
+//
+
+#ifndef INC_02_FIZZBUZZ_FIZZBUZZ_H
+#define INC_02_FIZZBUZZ_FIZZBUZZ_H
+
+#include <string>
+
+std::string fizzbuzz(int number);
+
+#endif //INC_02_FIZZBUZZ_FIZZBUZZ_H
